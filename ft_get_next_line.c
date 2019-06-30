9e3dbe8f3d9b29962b8839e6f7e33b9810e626ed
Zandile MASEKO <zamaseko@c4r11s10.wethinkcode.co.za>
2019-06-30 11:21:53 +0200

    Added get

--- ft_get_next_line.c
+++ ft_get_next_line.c
@@ -6,7 +6,7 @@
 /*   By: zamaseko <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/06/28 17:03:24 by zamaseko          #+#    #+#             */
-/*   Updated: 2019/06/30 11:17:47 by zamaseko         ###   ########.fr       */
+/*   Updated: 2019/06/30 11:21:05 by zamaseko         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -26,14 +26,11 @@ static int	ft_reader(char *s, char **line)
 			*line = ft_strnew(s, 0, i);
 			read = ft_strdup(*s[i + 1]);
 			*s = read;
-			if (s[i] == '\0')	
+			if (s[i] == '\0')
 				ft_strdel(s);
 		}
-		else 
+		else
 			ft_strdel(s);
 	}
 	return (1);
-}
-int	get_next_line(const int fd, char **line)
-{
 }
