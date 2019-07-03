3737595ba7ab4119312209fe051c4d71cfa3ff8b
Zandile MASEKO <zamaseko@c4r11s10.wethinkcode.co.za>
2019-07-03 17:05:40 +0200

    Added GNL.h

--- get_next_line.h
+++ get_next_line.h
@@ -6,7 +6,7 @@
 /*   By: zamaseko <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/07/01 15:55:03 by zamaseko          #+#    #+#             */
-/*   Updated: 2019/07/01 17:05:13 by zamaseko         ###   ########.fr       */
+/*   Updated: 2019/07/03 17:04:05 by zamaseko         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,7 +14,10 @@
 # define GET_NEXT_LINE_H
 # include <unistd.h>
 # include <stdlib.h>
-# include <>
-# include <>
+# include <fcntl.h>
+
+# define BUF_SIZE 32
+
+int	get_next_line(const int fd, char **line);
 
 #endif 
