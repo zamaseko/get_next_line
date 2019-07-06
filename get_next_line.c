2055120d4283764b048ccbdd306a8345d2e85df8
Zandile MASEKO <zamaseko@c4r11s10.wethinkcode.co.za>
2019-07-06 06:29:57 +0200

    Added GNL

--- get_next_line.c
+++ get_next_line.c
@@ -6,57 +6,107 @@
 /*   By: zamaseko <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/07/02 11:35:55 by zamaseko          #+#    #+#             */
-/*   Updated: 2019/07/05 17:29:03 by zamaseko         ###   ########.fr       */
+/*   Updated: 2019/07/06 06:24:16 by zamaseko         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "get_next_line.h"
+//static int ft_holder(char **s, char **line, int fd)
+/*{
+	char *stmp;
+	int i;
 
+	i = 0;
+	*line = ft_strsub(s[fd], 0, i);
+	stmp = ft_strdup(s[fd] + i + 1);
+	free(s[fd]);
+	s[fd] = stmp;
+	if (s[fd] == '\0')
+		ft_strdel(&s[fd]);
+	return (0);
+}*/
+// int	reader(i)
 static int	getmealine(char **s, char **line, int fd)
 {
+	char *stmp;
 	int		i;
-	char	*stmp;
-	
+
 	i = 0;
 	{
 		while (s[fd][i] != '\0' && s[fd][i] != '\n')
 			i++;
 		if (s[fd][i] == '\n')
 		{
+	//		ft_holder(&s[fd], line, fd);
 			*line = ft_strsub(s[fd], 0, i);
-			stmp = ft_strdup(s[fd] + i + 1);
+			stmp =  ft_strdup(s[fd] + i + 1);
 			free(s[fd]);
-			stmp = s[fd];
+			s[fd] = stmp;
+			if (s[fd] == '\0')
+				ft_strdel(&s[fd]);
 		}
-		if (s[i] == '\0')
-		ft_strdel(&s[fd]);
+		else if (s[fd] == '\0')
+		ft_strdup(s[fd]);
+		free(*s);
 	}
 	return (1);
 }
 
-int	get_next_line(const int fd, char **line)
+int be_safe(const int fd, char *line)
 {
-	int reader;
-	static char *s[0];
+	int checker;
 	char buf[BUF_SIZE + 1];
-	char *tmps;
 
 	if (fd < 0 || !line)
 		return (-1);
-	while ((reader = (read(fd, buf, BUF_SIZE) < 0)))
+	if ((checker = (read(fd, buf, BUF_SIZE < 0))))
 		return (-1);
-		while (buf[fd] == '\0')
-		{
-			if (s[fd] == NULL)
-			s[fd] = ft_strnew(1);
-			tmps = ft_strjoin(s[fd], buf);
-			free(s[fd]);
-			if (ft_strchr(s[fd], '\n'))
-				break ;
-		}
-		if (reader < 0)
-			return (-1);
-		else if (reader == 0 && s[fd] == NULL && s[fd] == '\0')
-			return (0);
-		return (getmealine(s, line, fd));
 }
+
+int	get_next_line(const int fd, char **line)
+{
+	int checker;
+	static char s[1024];
+	char buf[BUF_SIZE + 1];
+	char *tmps;
+
+	checker = read(fd, buf, BUF_SIZE);
+//	if (fd < 0 || !line)
+//		return (-1);
+//	if ((reader = (read(fd, buf, BUF_SIZE) < 0)))
+//		return (-1);
+	be_safe(fd, *line);
+	while (buf[fd] == '\0')
+	{
+		if (s[fd] == 0)
+		tmps = ft_strjoin(&s[fd], buf);
+		free(&s[fd]);
+		if (ft_strchr(&s[fd], '\n'))
+			break ;
+	}
+	if (checker < 0)
+		return (-1);
+	else if (checker == 0 && s[fd] == 0 && s[fd] == '\0')
+		return (0);
+	return (getmealine(s, line, fd));
+}
+/*
+#include <stdio.h>
+#include <fcntl.h>
+int			main()
+{
+	char	*line = NULL;
+	int 	fd;
+	int 	output;
+
+	fd = open( "bible.txt", O_RDONLY);
+	output = 1;
+	while (output > 0)
+	{
+		output = get_next_line(fd, &line);
+		printf("[%d] line: %s", output, line);
+		ft_strdel(&line);
+		line = NULL;
+	}
+	return (0);
+}*/
--- get_next_line.h
+++ get_next_line.h
@@ -6,17 +6,16 @@
 /*   By: zamaseko <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/07/01 15:55:03 by zamaseko          #+#    #+#             */
-/*   Updated: 2019/07/04 08:55:11 by zamaseko         ###   ########.fr       */
+/*   Updated: 2019/07/05 17:31:40 by zamaseko         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #ifndef GET_NEXT_LINE_H
 # define GET_NEXT_LINE_H
-# include <unistd.h>
 # include "libft/libft.h"
 
 # define BUF_SIZE 32
 
 int	get_next_line(const int fd, char **line);
 
-#endif 
+#endif
