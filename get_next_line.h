3f55bffe9bd5d60b948152619b385abdd9fc32a0
Zandile MASEKO <zamaseko@c4r11s10.wethinkcode.co.za>
2019-07-08 16:38:04 +0200

    Added hFile

--- get_next_line.h
+++ get_next_line.h
@@ -6,15 +6,15 @@
 /*   By: zamaseko <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/07/01 15:55:03 by zamaseko          #+#    #+#             */
-/*   Updated: 2019/07/05 17:31:40 by zamaseko         ###   ########.fr       */
+/*   Updated: 2019/07/08 16:37:06 by zamaseko         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #ifndef GET_NEXT_LINE_H
 # define GET_NEXT_LINE_H
-# include "libft/libft.h"
-
 # define BUF_SIZE 32
+
+# include "libft/libft.h"
 
 int	get_next_line(const int fd, char **line);
 
