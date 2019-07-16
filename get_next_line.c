8e83d5410e27988f313761b06ba3c7c06f82f970
Zandile MASEKO <zamaseko@c4r11s10.wethinkcode.co.za>
2019-07-16 12:46:50 +0200

    Added new new

--- get_next_line.c
+++ get_next_line.c
@@ -6,20 +6,20 @@
 /*   By: zamaseko <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/07/11 10:10:17 by zamaseko          #+#    #+#             */
-/*   Updated: 2019/07/16 11:23:20 by zamaseko         ###   ########.fr       */
+/*   Updated: 2019/07/16 12:46:31 by zamaseko         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "get_next_line.h"
-int		in_read(const int fd, char *sr)
+int			in_read(const int fd, char *sr)
 {
-	int	ih;
+	int		ih;
 	char	buf[BUFF_SIZE + 1];
 
 	ih = read(fd, buf, BUFF_SIZE);
 	if (fd < 0 || BUFF_SIZE < 1)
 		return (0);
-	if (sr = NULL)
+	if (!sr)
 		ft_strnew(1);
 	while (!ft_strchr(sr, '\n'))
 	{
@@ -33,3 +33,10 @@ int		in_read(const int fd, char *sr)
 	return (1);
 }
 
+int		get_next_line(const int fd, char **line)
+{
+	char *s[1024];
+
+	if (fd < 0 || !line)
+		return (-1);
+}
